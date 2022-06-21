2d20ca524316306e4a0290288e2b25c6fdd080c4
KamiCreed <9517086+KamiCreed@users.noreply.github.com>
2022-06-21 02:36:43 -0700

    Map region names

--- common.py
+++ common.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 import pandas as pd
 
-def combine_csvs(src, ind_col=0):
-    return pd.concat([pd.read_csv(f, index_col=ind_col) for f in src])
+region_mapping = dict(Africa_Cape_Town='Cape Town', Asia_Pacific_Mumbai='Mumbai', Asia_Pacific_Singapore='Singapore',
+                     Asia_Pacific_Sydney='Sydney', Asia_Pacific_Tokyo='Tokyo', Europe_London='London',
+                     Middle_East_Bahrain='Bahrain', South_America_Sao_Paulo='Sao Paulo', US_West_N_California='N. California')
+
+def combine_csvs(src, ind_col=0, header='infer'):
+    return pd.concat([pd.read_csv(f, index_col=ind_col, header=header) for f in src])
